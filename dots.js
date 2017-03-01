59fd45aa0628ba5872d0d50c5663b0b1379ad989
Issac-Kim <ikim1@stuy.edu>
2017-02-28 19:16:35 -0500

    finish connect dots using svg

--- dots.js
+++ dots.js
@@ -1,5 +1,8 @@
 var svg = document.getElementById("drawing");
 
+var prev = false;
+var prevX, prevY;
+
 var draw_dot = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
@@ -7,8 +10,31 @@ var draw_dot = function(e){
     dot.setAttribute("fill", "blue");
     dot.setAttribute("cx", x);
     dot.setAttribute("cy", y);
-    dot.setAttribute("r", "50");
+    dot.setAttribute("r", "25");
     svg.appendChild(dot);
+    if(prev){
+	var connect = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	connect.setAttribute("style", "stroke:rgb(255,0,0);stroke-width:2" );
+	connect.setAttribute("x1", x);
+	connect.setAttribute("x2", prevX);
+	connect.setAttribute("y1", y);
+	connect.setAttribute("y2", prevY);
+	svg.append(connect);
+    }
+    prevX = x;
+    prevY = y;
+    prev = true;
 }
 
 svg.addEventListener("click", draw_dot);
+
+var clear_button = document.getElementById("clear");
+
+var clear = function(e){
+    prev = false;
+    while (svg.lastChild) {
+	svg.removeChild(svg.lastChild);
+    }
+}
+
+clear_button.addEventListener("click", clear);
